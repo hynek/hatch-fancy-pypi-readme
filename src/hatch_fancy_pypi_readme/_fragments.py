e078f2c4a5fbd8c8aea26a42e142e15ec070518c
Hynek Schlawack <hs@ox.cx>
2024-10-08 10:31:20 +0200

    Drop 3.7

--- src/hatch_fancy_pypi_readme/_fragments.py
+++ src/hatch_fancy_pypi_readme/_fragments.py
@@ -5,17 +5,10 @@
 from __future__ import annotations
 
 import re
-import sys
 
 from dataclasses import dataclass
 from pathlib import Path
-from typing import ClassVar, Iterable
-
-
-if sys.version_info >= (3, 8):
-    from typing import Protocol
-else:
-    from typing_extensions import Protocol
+from typing import ClassVar, Iterable, Protocol
 
 from .exceptions import ConfigurationError
 
